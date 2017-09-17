8b4cb2f64b3070e77176ca3d0714c18dc1b0cdb8
David Salek <salekd@gmail.com>
2017-09-17 22:11:50 +0200

    cleanup

--- judy_light.py
+++ judy_light.py
@@ -1,13 +1,10 @@
 import judy
-import os
 import re
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 # Set GPIO 18 for output.
-GPIO.setup(18, GPIO.OUT)
-#os.system('echo "18" > /sys/class/gpio/export')
-#os.system('echo "out" > /sys/class/gpio/gpio18/direction')
+GPIO.setup(18, GPIO.OUT, initial=GPIO.HIGH)
 
 # Use USB microphone and
 # specify the Pocketshpinx dictionary for the speech recognition.
@@ -29,18 +26,14 @@ def handle(phrase):
     """
     print('Heard: {}'.format(phrase))
 
-    #os.system('if [ `cat /sys/class/gpio/gpio18/value` -eq "0" ]; then echo "1" > /sys/class/gpio/gpio18/value; else echo "0" > /sys/class/gpio/gpio18/value; fi')
     if bool(re.search(r'\blight on\b', phrase, re.IGNORECASE)):
-        #os.system('echo "1" > /sys/class/gpio/gpio18/value')
-        GPIO.output(18, GPIO.HIGH)
-    if bool(re.search(r'\blight off\b', phrase, re.IGNORECASE)):
-        #os.system('echo "0" > /sys/class/gpio/gpio18/value')
         GPIO.output(18, GPIO.LOW)
+    if bool(re.search(r'\blight off\b', phrase, re.IGNORECASE)):
+        GPIO.output(18, GPIO.HIGH)
 
 try:
     # Call "Judy" to get her attention, then say something (within the vocabulary).
     judy.listen(vin, vout, handle)
 except KeyboardInterrupt:
     # Release the GPIO pin.
-    #os.system('udo echo "18" > /sys/class/gpio/unexport')
     GPIO.cleanup()
