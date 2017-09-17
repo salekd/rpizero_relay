c0578b9251c105077461518d08be3f8149d35060
David Salek <salekd@gmail.com>
2017-09-17 22:32:19 +0200

    cleanup

--- Light.py
+++ Light.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8-*-
 import re
-import RPi.GPIO as GPIO
 import os
 
-WORDS = ["LIGHT", "LIGHTS"]
+WORDS = ["LIGHT"]
 
 
 def handle(text, mic, profile):
     """
-        Responds to the keyword 'light' by switching on/off GPIO pins
+        Responds to the keyword 'light' by switching on/off the GPIO pin
         connected to a light, for example through a relay.
 
         Arguments:
@@ -17,16 +16,14 @@ def handle(text, mic, profile):
         profile -- contains information related to the user (e.g., phone
                    number)
     """
-    print("Light")
     os.system('if [ `cat /sys/class/gpio/gpio18/value` -eq "0" ]; then echo "1" > /sys/class/gpio/gpio18/value; else echo "0" > /sys/class/gpio/gpio18/value; fi')
 
 
 def isValid(text):
     """
-        Returns True if the input is related to the meaning of life.
+        Returns True if the input is related to 'light'.
 
         Arguments:
         text -- user-input, typically transcribed speech
     """
-    #return True
     return bool(re.search(r'\blight\b', text, re.IGNORECASE))
