5aec257f92fc38bda2b2fa146927213242a2605f
David Salek <salekd@gmail.com>
2017-09-18 00:32:23 +0200

    file path

--- judy_light.py
+++ judy_light.py
@@ -1,16 +1,19 @@
 import judy
 import re
 import RPi.GPIO as GPIO
+from pathlib import Path
 
 GPIO.setmode(GPIO.BCM)
 # Set GPIO 18 for output.
 GPIO.setup(18, GPIO.OUT, initial=GPIO.HIGH)
 
+file_path = Path(__file__).resolve().parent
+
 # Use USB microphone and
 # specify the Pocketshpinx dictionary for the speech recognition.
 vin = judy.VoiceIn(adcdev='plughw:0,0',
-                   lm='7369.lm',
-                   dict='7369.dic')
+                   lm=str(file_path / '7369.lm'),
+                   dict=str(file_path /'7369.dic'))
 
 # Audio output configuration is needed by Judy.
 # Nothing will be heard from Raspberry Pi Zero unless
