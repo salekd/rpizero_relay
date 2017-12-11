3b427b60f0b7a52b4441600a71cc6608a178c8f2
David Salek <salekd@gmail.com>
2017-12-11 21:06:35 +0100

    corrected GPIO pin number

--- button.py
+++ button.py
@@ -10,10 +10,10 @@ try:
     while True:
         GPIO.wait_for_edge(14, GPIO.FALLING)  
         # If we are here, an edge was recognized
-        # Debounce for 200 ms
-        sleep(0.2)
+        # Debounce for 5 ms
+        sleep(0.005)
         # Only react to valid edges
-        if GPIO.input(18) == 0:
+        if GPIO.input(14) == 0:
             GPIO.output(18, not GPIO.input(18))
             sleep(1)
               
